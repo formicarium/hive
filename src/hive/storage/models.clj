6aad44e645e06fca769705cb923a893ee614a63c
Luiz Santos <luiz.g.s07@gmail.com>
2018-03-18 16:51:04 -0300

    tweaking schema

--- src/hive/storage/models.clj
+++ src/hive/storage/models.clj
@@ -1,5 +1,7 @@
 (ns hive.storage.models
-  (:require [schema.core :as s]))
+  (:require [schema.core :as s]
+            [clj-time.types :as time.types])
+  (:import org.joda.time.DateTime))
 
 (def EventTypes (s/enum :new :heartbeat :test))
 (def PortTypes (s/enum :http :kafka))
@@ -9,3 +11,9 @@
                        :synchronous? s/Bool ;may not be useful
                        :port         PortTypes}
             :payload  s/Str})
+
+(def ServiceStatuses (s/enum :ok :unresponsive :dead))
+
+(def Service {:name           s/Keyword
+              :last-timestamp DateTime
+              :status         ServiceStatuses})
